a19618ca608325622d40dcc6932c3711ef6bed9e
shamofu <17104318+shamofu@users.noreply.github.com>
2021-04-14 01:01:17 +0900

    Fix vite.config.js

--- src-vite/vite.config.js
+++ src-vite/vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import resolve from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
