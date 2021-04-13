3280b11a3ca750530f44f017cfc32a9d38c25f46
shamofu <17104318+shamofu@users.noreply.github.com>
2021-04-14 01:13:20 +0900

    Tweak vite.config.js

--- src-vite/vite.config.js
+++ src-vite/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
-import resolve from 'path'
+const { resolve } = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
